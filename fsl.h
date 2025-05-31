1c185097f694499de25958f253f314a731b7f364
Keith A. Lewis <kal@kalx.net>
2025-05-31 15:59:00 -0400

    hw2

--- fsl.h
+++ fsl.h
@@ -1,3 +1,7 @@
 // fsl.h - Financial Software Library
 // #pragma once
 #include "xll24/include/xll.h"
+
+#ifndef CATEGORY
+#define CATEGORY L"FSL"
+#endif
--- xll_fsl.cpp
+++ xll_fsl.cpp
@@ -4,6 +4,7 @@
 using namespace xll;
 
 // Standard normal cumulative distribution function P(Z <= z).
+// https://en.wikipedia.org/wiki/Error_function#Cumulative_distribution_function
 double normal_cdf(double z)
 {
 	// Cumulative distribution function for the standard normal distribution
@@ -16,15 +17,15 @@ int test_normal_cdf()
 		for (double z : zs) {
 			double cdf = normal_cdf(z);
 			// compare with Excel builtin
-			double cdf_ = Num(Excel(xlfNormsdist, z));
-			assert(fabs(cdf - cdf_) <= 1e-8);
+			double cdf_ = asNum(Excel(xlfNormsdist, z));
+			assert(fabs(cdf - cdf_) <= std::numeric_limits<double>::epsilon());
 		}
 	}
 
 	return 0;
 }
 
-// Factor out platform-independent code.
+// Factor out code independent of Excel.
 // F = f exp(sZ - s^2/2) <= k if and only if Z <= (log(k/f) + s^2/2)/s
 double fsl_black_moneyness(double f, double s, double k)
 {
@@ -38,16 +39,35 @@ double fsl_black_moneyness(double f, double s, double k)
 int test_fsl_black_moneyness()
 {
 	{
+		// Test exception.
+		double data[][3] = {
+			// f, s, k
+			{-1, 1, 1},
+			{1, -1, 1},
+			{1, 1, -1}
+		};
+		for (auto [f, s, k] : data) {
+			bool thrown = false;
+			try {
+				fsl_black_moneyness(f, s, k);
+			}
+			catch (const std::exception&) {
+				thrown = true;
+			}
+			assert(thrown);
+		}
+	}
+	{
 		double data[][4] = {
 			// f, s, k, z
-			{100, .1, 100, 0.05},
+			{100, .1, 100, 0.05000000000000001},
 			{100, .1, 90, -1.0036051565782627},
 			{100, .1, 110, 1.0031017980432493},
 			// ...more tests here...
 		};
 		for (auto [f, s, k, z] : data) {
 			double z_ = fsl_black_moneyness(f, s, k);
-			assert(fabs(z - z_) <= 1e-8);
+			assert(z == z_);
 		}
 	}
 
@@ -61,7 +81,7 @@ AddIn xai_black_moneyness(
 		Arg(XLL_DOUBLE, L"s", L"is the vol."),
 		Arg(XLL_DOUBLE, L"k", L"is the strike price of the option."),
 		})
-		.Category(L"FSL")
+		.Category(CATEGORY)
 	.FunctionHelp(L"Return the Black moneyness of an option, defined as (log(k/f) + s^2/2)/s.")
 );
 double WINAPI xll_black_moneyness(double f, double s, double k)
@@ -71,6 +91,7 @@ double WINAPI xll_black_moneyness(double f, double s, double k)
 	double result = std::numeric_limits<double>::quiet_NaN();
 
 	try {
+		// Collect arguments from Excel and call C++.
 		result = fsl_black_moneyness(f, s, k);
 	}
 	catch (const std::exception& ex) {
@@ -83,9 +104,6 @@ double WINAPI xll_black_moneyness(double f, double s, double k)
 // E[max(k - F, 0)] = k P(Z <= z) - f P(Z + s <= z)
 double fsl_black_put_value(double f, double s, double k)
 {
-	if (f <= 0 || s <= 0 || k <= 0) {
-		throw std::runtime_error("f, s, and k must be positive");
-	}
 	double z = fsl_black_moneyness(f, s, k);
 
 	return k * normal_cdf(z) - f * normal_cdf(z - s);
@@ -100,7 +118,7 @@ int test_fsl_black_put_value()
 		};
 		for (auto [f, s, k, p] : data) {
 			double p_ = fsl_black_put_value(f, s, k);
-			assert(fabs(p - p_) <= 1e-8);
+			assert(p == p_);
 		}
 	}
 
@@ -114,7 +132,7 @@ AddIn xai_black_put_value(
 		Arg(XLL_DOUBLE, L"s", L"is the vol."),
 		Arg(XLL_DOUBLE, L"k", L"is the strike price of the option."),
 		})
-		.Category(L"FSL")
+		.Category(CATEGORY)
 	.FunctionHelp(L"Return the forward Black put value of an option E[max{k - F, 0}]"
 		" where F = f exp(s Z - s^2/2) and Z is a standard normal random variable.")
 );
@@ -134,11 +152,9 @@ double WINAPI xll_black_put_value(double f, double s, double k)
 }
 
 // (d/df) E[max(k - F, 0)] = E[-1(F <= k) dF/df]
+// dF/df = exp(s Z - s^2/2).
 double fsl_black_put_delta(double f, double s, double k)
 {
-	if (f <= 0 || s <= 0 || k <= 0) {
-		throw std::runtime_error("f, s, and k must be positive");
-	}
 	double z = fsl_black_moneyness(f, s, k);
 
 	return - f * normal_cdf(z - s);
@@ -148,12 +164,12 @@ int test_fsl_black_put_delta()
 	{
 		double data[][4] = {
 			// f, s, k, p 
-			{100, .1, 100, 3.9877611676744920},
+			{100, .1, 100, -48.006119416162754},
 			// ...more tests here...
 		};
 		for (auto [f, s, k, p] : data) {
 			double p_ = fsl_black_put_delta(f, s, k);
-			assert(fabs(p - p_) <= 1e-8);
+			assert(p == p_);
 		}
 	}
 
@@ -167,7 +183,7 @@ AddIn xai_black_put_delta(
 		Arg(XLL_DOUBLE, L"s", L"is the vol."),
 		Arg(XLL_DOUBLE, L"k", L"is the strike price of the option."),
 		})
-		.Category(L"FSL")
+		.Category(CATEGORY)
 	.FunctionHelp(L"Return the forward Black put delta of an option E[max{k - F, 0}]"
 		" where F = f exp(s Z - s^2/2) and Z is a standard normal random variable.")
 );
@@ -186,28 +202,77 @@ double WINAPI xll_black_put_delta(double f, double s, double k)
 	return result;
 }
 
-// Black-Scholes/Merton S_t = S exp((r - s^2/2) t + sigma B_t) where B_t is a standard Brownian motion.
+// Black-Scholes/Merton S_t = s0 exp((r - sigma^2/2) t + sigma B_t) where B_t is a standard Brownian motion.
 // Put value is exp(-r t) E[max{k - S_t, 0}]
-// If F = S_t then f = S exp(r t) and s = sigma sqrt(t).
-
+// If F = S_t then f = s0 exp(r t) and s = sigma sqrt(t).
 // Convert Black-Scholes/Merton parameters to Black model.
-std::tuple<double, double, double> fsl_bsm_black(double r, double S, double sigma, double t)
+std::tuple<double, double, double> fsl_bsm_black(double r, double s0, double sigma, double t)
 {
-	if (S <= 0 || sigma <= 0 || t <= 0) {
-		throw std::runtime_error("S, sigma, and t must be positive");
+	if (s0 <= 0 || sigma <= 0 || t <= 0) {
+		throw std::runtime_error("s0, sigma, and t must be positive");
 	}
 	double D = exp(-r * t); // Discount factor
-	double f = S / D; // Forward price
+	double f = s0 / D; // Forward price
 	double s = sigma * sqrt(t); // Volatility for Black model
 
 	return { D, f, s };
 }
-double fsl_bsm_put_value(double r, double S, double sigma, double t, double k)
+int test_fsl_bsm_black()
 {
-	auto [D, f, s] = fsl_bsm_black(r, S, sigma, t);
+	{
+		double data[][7] = {
+			// r, s0, sigma, t, D, f, s
+			{0.05, 100, 0.2, 1, exp(-0.05*1), 100/exp(-0.05*1), 0.2*sqrt(1)},
+			{0.03, 50, 0.1, 2, 0.94176453358424872, 53.091827327267978, 0.14142135623730953},
+			// ...more tests here...
+		};
+		for (auto [r, s0, sigma, t, D, f, s] : data) {
+			auto [D_, f_, s_] = fsl_bsm_black(r, s0, sigma, t);
+			assert(D == D_);
+			assert(f == f_);
+			assert(s == s_);
+		}
+	}
+	return 0;
+}
+
+double fsl_bsm_put_value(double r, double s0, double sigma, double t, double k)
+{
+	auto [D, f, s] = fsl_bsm_black(r, s0, sigma, t);
 
 	return D * fsl_black_put_value(f, s, k);
 }
+int test_fsl_bsm_put_value()
+{
+	{
+		double data[][6] = {
+			// r, s0, sigma, t, k, p
+			{0.05, 100, 0.2, 1, 100, 5.5735260222569671},
+			{0.03, 50, 0.1, 2, 55, 3.8565888892016207},
+			// ...more tests here...
+		};
+		for (auto [r, s0, sigma, t, k, p] : data) {
+			double p_ = fsl_bsm_put_value(r, s0, sigma, t, k);
+			assert(fabs(p - p_) <= std::numeric_limits<double>::epsilon());
+		}
+	}
+	
+	return 0;
+}
+//
+// TODO: Implement BSM.PUT.VALUE
+// 
+
+// B-S/M delta is d/dS exp(-r t) E[max(k - S_t, 0)] = E[-1(S_t <= k) dS_t/dS]
+// dS_t/dS = exp((r - sigma^2/2) t + sigma B_t) ~ exp(r t) exp(s Z - s^2/2)
+
+//
+// TODO: Implement fsl_bsm_put_delta in C++
+// 
+// TODO: Implement test_fsl_bsm_put_delta in C++
+//
+// TODO: Implement the BSM.PUT.DELTA add-in
+//
 
 // Run tests on xlAutoOpen
 Auto<Open> xao_fsl_test([]() {
@@ -215,6 +280,10 @@ Auto<Open> xao_fsl_test([]() {
 		test_normal_cdf();
 		test_fsl_black_moneyness();
 		test_fsl_black_put_value();
+		test_fsl_black_put_delta();
+		test_fsl_bsm_black();
+		test_fsl_bsm_put_value();
+		//test_fsl_bsm_put_delta();
 	}
 	catch (const std::exception& ex) {
 		XLL_ERROR(ex.what());
